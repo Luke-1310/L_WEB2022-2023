412e26b8a421139e350a535ffda8e6691d884b22
Luke-1310 <priviteraluca2001@gmail.com>
2023-06-28 18:03:26 +0200

    validator implemented & libri fixed

---
+++ 3-homework/res/PHP/validator.php
@@ -0,0 +1,26 @@
+<?php
+// Percorso del file XML da validare
+$xmlFile = '../XML/libri.xml';
+
+// Percorso del file schema XSD
+$xsdFile = '../XML/libro.xsd';
+
+// Crea un oggetto DOMDocument
+$dom = new DOMDocument();
+
+// Carica il file XML
+$dom->load($xmlFile);
+
+// Abilita la validazione dello schema
+$dom->schemaValidate($xsdFile);
+
+// Se la validazione ha successo, il documento XML è conforme allo schema
+echo "Il documento XML è valido secondo lo schema specificato.";
+
+// Altrimenti, se la validazione fallisce, mostra gli eventuali errori
+libxml_use_internal_errors(true);
+$errors = libxml_get_errors();
+foreach ($errors as $error) {
+    echo "Errore di validazione: " . $error->message . "\n";
+}
+?>
