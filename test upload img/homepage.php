4c5c781da4c6970bf320f425d5b1d5717b7f1da8
Luke-1310 <priviteraluca2001@gmail.com>
2023-06-28 16:23:36 +0200

    a new option to upload an image

---
+++ test upload img/homepage.php
@@ -0,0 +1,20 @@
+<form action="upload.php" method="POST" enctype="multipart/form-data">
+  <input type="file" name="image" accept="image/*">
+  <input type="submit" value="Carica immagine">
+</form>
+
+<style>
+  /* Aggiungi stili personalizzati qui */
+  form {
+    margin-top: 20px;
+  }
+
+  input[type="submit"] {
+    background-color: #4CAF50;
+    color: white;
+    padding: 10px 20px;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
+  }
+</style>
